b985f1e3f6a25c12e464baac966f3cb9c0d3ba18
Paul Crowder <paul.crowder@blackbaud.com>
2020-08-28 14:13:36 -0400

    Added modern theme styles to repeater (#194)

--- e2e/repeater.e2e-spec.ts
+++ e2e/repeater.e2e-spec.ts
@@ -1,6 +1,7 @@
 import {
   expect,
-  SkyHostBrowser
+  SkyHostBrowser,
+  SkyVisualThemeSelector
 } from '@skyux-sdk/e2e';
 
 import {
@@ -9,107 +10,155 @@ import {
 } from 'protractor';
 
 describe('Repeater', () => {
-  it('should match previous repeater screenshot', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater');
-    expect('#screenshot-repeater').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater'
+  let currentTheme: string;
+  let currentThemeMode: string;
+
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function runTests(): void {
+    it('should match previous repeater screenshot', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater');
+
+      expect('#screenshot-repeater').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater')
+      });
     });
+
+    it('should match previous repeater screenshot when an item is active', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater-with-active-item');
+
+      expect('#screenshot-repeater-with-active-item').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-with-active-item')
+      });
+    });
+
+    it('should match previous repeater screenshot when all are collapsed', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater-collapsed');
+
+      expect('#screenshot-repeater-collapsed').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-collapsed')
+      });
+    });
+
+    it('should match previous repeater screenshot in single mode', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater-single');
+
+      expect('#screenshot-repeater-single').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-single')
+      });
+    });
+
+    it('should match previous repeater screenshot in multiple mode', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater-multiple');
+
+      expect('#screenshot-repeater-multiple').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-multiple')
+      });
+    });
+
+    it('should match previous repeater screenshot in max-width container', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater-max-width');
+
+      expect('#screenshot-repeater-max-width').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-max-width')
+      });
+    });
+
+    it('should match previous repeater screenshot with a context menu', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater-context-menu');
+
+      expect('#screenshot-repeater-context-menu').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-context-menu')
+      });
+    });
+
+    it('should match previous repeater screenshot when selectable', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater-selectable');
+
+      expect('#screenshot-repeater-selectable').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-selectable')
+      });
+    });
+
+    it('should match previous repeater screenshot when selectable', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater-selectable-context');
+
+      expect('#screenshot-repeater-selectable-context').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-selectable-and-context')
+      });
+    });
+
+    it('should match previous repeater screenshot when reorderable', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-repeater-reorderable');
+
+      expect('#screenshot-repeater-reorderable').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-reorderable')
+      });
+    });
+
+    it('should match previous repeater screenshot with an inline delete', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-last-repeater-item');
+
+      await element.all(
+        by.css('#screenshot-repeater-inline-delete .sky-dropdown-button')
+      ).get(0).click();
+
+      await element(by.css('#inline-delete-trigger-standard')).click();
+
+      await element.all(
+        by.css('#screenshot-repeater-inline-delete .sky-dropdown-button')
+      ).get(1).click();
+
+      await element(by.css('#inline-delete-trigger-active')).click();
+
+      await element(by.css('#screenshot-repeater-inline-delete')).click();
+
+      expect('#screenshot-repeater-inline-delete').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('repeater-inline-delete')
+      });
+    });
+  }
+
+  beforeEach(async () => {
+    currentTheme = undefined;
+    currentThemeMode = undefined;
+
+    await SkyHostBrowser.get('visual/repeater');
+    await SkyHostBrowser.setWindowBreakpoint('lg');
   });
 
-  it('should match previous repeater screenshot when an item is active', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater-with-active-item');
-    expect('#screenshot-repeater-with-active-item').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-with-active-item'
+  runTests();
+
+  describe('when modern theme', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'light');
     });
+
+    runTests();
   });
 
-  it('should match previous repeater screenshot when all are collapsed', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater-collapsed');
-    expect('#screenshot-repeater-collapsed').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-collapsed'
+  describe('when modern theme in dark mode', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'dark');
     });
-  });
 
-  it('should match previous repeater screenshot in single mode', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater-single');
-    expect('#screenshot-repeater-single').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-single'
-    });
-  });
-
-  it('should match previous repeater screenshot in multiple mode', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater-multiple');
-    expect('#screenshot-repeater-multiple').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-multiple'
-    });
-  });
-
-  it('should match previous repeater screenshot in max-width container', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater-max-width');
-    expect('#screenshot-repeater-max-width').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-max-width'
-    });
-  });
-
-  it('should match previous repeater screenshot with a context menu', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater-context-menu');
-    expect('#screenshot-repeater-context-menu').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-context-menu'
-    });
-  });
-
-  it('should match previous repeater screenshot when selectable', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater-selectable');
-    expect('#screenshot-repeater-selectable').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-selectable'
-    });
-  });
-
-  it('should match previous repeater screenshot when selectable', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater-selectable-context');
-    expect('#screenshot-repeater-selectable-context').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-selectable-and-context'
-    });
-  });
-
-  it('should match previous repeater screenshot when reorderable', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-repeater-reorderable');
-    expect('#screenshot-repeater-reorderable').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-reorderable'
-    });
-  });
-
-  it('should match previous repeater screenshot with an inline delete', (done) => {
-    SkyHostBrowser.get('visual/repeater');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-last-repeater-item');
-    element.all(by.css('#screenshot-repeater-inline-delete .sky-dropdown-button')).get(0).click();
-    element(by.css('#inline-delete-trigger-standard')).click();
-    element.all(by.css('#screenshot-repeater-inline-delete .sky-dropdown-button')).get(1).click();
-    element(by.css('#inline-delete-trigger-active')).click();
-    element(by.css('#screenshot-repeater-inline-delete')).click();
-    expect('#screenshot-repeater-inline-delete').toMatchBaselineScreenshot(done, {
-      screenshotName: 'repeater-inline-delete'
-    });
+    runTests();
   });
 });
